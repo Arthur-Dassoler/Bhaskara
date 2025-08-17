cb52b9549a96ba2a1a72a570e0a0ea5b3a9dd6e2
Arthur-Dassoler <dassoler.arthur@gmail.com>
2025-08-17 13:52:07 -0300

    delta = 0

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function Calculo() {
     let b = document.getElementById("termoB").value
     let c = document.getElementById("termoC").value
 
-    const delta = ((b * b) -4 * a * c)
-    const resultado1 = (-b + (delta**0.5))/2 * a
-    const resultado2 = (-b - (delta**0.5))/2 * a
+    const delta = ((b * b) - 4 * a * c)
+    const resultado1 = (-b + (delta ** 0.5)) / (2 * a)
+    const resultado2 = (-b - (delta ** 0.5)) / (2 * a)
 
-    document.getElementById("resultado").innerHTML = "As raizes dessa equação são " + resultado1 + " e " + resultado2
+    if (isNaN(resultado1 && resultado2)) {
+        document.getElementById("resultado").innerHTML = "Essa equação não possui raízes reais";
+    }
+    else if (delta === 0) {
+        document.getElementById("resultado").innerHTML = "Essa equação possui somente uma raiz real: " + resultado1
+    }
+    else {
+        document.getElementById("resultado").innerHTML = "As raizes dessa equação são " + resultado1 + " e " + resultado2;
+    }
 }
